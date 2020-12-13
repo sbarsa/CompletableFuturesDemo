550923fc6ca3fb81b0ea492f42b485c19d614b28
Sergiu Barsa <sergiu.barsa@qhrtech.com>
2020-12-12 18:57:27 -0800

    fix tests

--- src/test/java/sergiu/barsa/demo/RequestServiceTest.java
+++ src/test/java/sergiu/barsa/demo/RequestServiceTest.java
@@ -1,6 +1,5 @@
 package sergiu.barsa.demo;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
@@ -18,8 +17,6 @@ class RequestServiceTest {
 
         String result = service.processRawNote( input );
 
-        Assertions.fail("failing so that we can see the failure in Github CI");
-
-        //assertEquals( expectedOutput, result, "Wrong result received during processing" );
+        assertEquals( expectedOutput, result, "Wrong result received during processing" );
     }
 }
